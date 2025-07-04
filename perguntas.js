80bf5b57459c4e1e69cc98c26fde445497a2b7e7
Luizedu2k09 <luiz.lima.pinto@escola.pr.gov.br>
2025-07-04 12:37:35 +0000

    Oingo Boingo brothers

--- perguntas.js
+++ perguntas.js
@@ -1,65 +1,55 @@
-criarCartao(
-    "Categoria",    
-    "Pergunta",  
-    "Resposta" 
-);
+
 
 criarCartao(
     "Q1",    
-    "o que é html?",  
-    "é a linguagem padrao da estrutura do codigo" 
-);
-
-criarCartao(
-    "Q2",    
     "o'que é programação?",  
     "é um conjunto de codigos que fazem o diposotivo executar tarefas especificas" 
 );
 
 criarCartao(
-    "Q3",    
+    "Q2",    
     "Qual a sigla do subcampo que processa linguagem natural?",  
     "PLN" 
 );
 
 criarCartao(
-    "Q4",    
+    "Q3",    
     "quando a programaçao surgiu??",  
     "1843" 
 );
 
 criarCartao(
-    "Q5",    
+    "Q4",    
     "Qual profissão abramge a programação?",  
     "desenvolvedores de software" 
 );
 
 criarCartao(
-    "Q6",    
+    "Q5",    
     "Qual assistente virtual mencionado ajuda em tarefas diárias?",  
     "Siri" 
 );
 
 criarCartao(
-    "Q7",    
+    "Q6",    
     "Qual termo refere-se ao preconceito em algoritmos?",  
     "Viés" 
 );
 
 criarCartao(
-    "Q8",    
+    "Q7",    
     "Para que tarefa carros autônomos usam IA?",  
     "Navegação" 
 );
 
 criarCartao(
-    "Q9",    
+    "Q8",    
     "Que subcampo combina IA e engenharia mecânica?",  
     "Robótica" 
 );
 
 criarCartao(
-    "Q10",    
+    "Q9",    
     "Em qual domínio a IA é usada para avaliação de riscos?",  
     "Finanças" 
 );
